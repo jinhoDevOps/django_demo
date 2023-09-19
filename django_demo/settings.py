639a772f110451e638fb0e1425bbaa0f23b96a5c
jinhoDevOps <jhok@n3cloud.co.kr>
2023-09-19 14:48:53 +0900

    feat: 설정변경ㅇ
    LANGUAGE_CODE = 'ko-kr'
    TIME_ZONE = 'Asia/Seoul'

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -115,10 +115,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ko-kr'
 
+TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
 
 USE_TZ = True
